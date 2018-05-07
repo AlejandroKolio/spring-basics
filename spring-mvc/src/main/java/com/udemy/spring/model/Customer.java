a4a0acc1eb2d6a220279b3cb244f333169d49f15
Alexander Shakhov <alexander.shakhov@sperasoft.com>
2018-05-07 19:11:08 +0300

    add hibernate validator dependencies

---
+++ spring-mvc/src/main/java/com/udemy/spring/model/Customer.java
@@ -0,0 +1,16 @@
+package com.udemy.spring.model;
+
+import lombok.Getter;
+import lombok.Setter;
+
+/**
+ * @author alexander.shakhov on 07.05.2018 19:06
+ * @project spring-basics
+ * @description
+ */
+@Getter
+@Setter
+public class Customer {
+    private String firstName;
+    private String lastName;
+}
