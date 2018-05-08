b96ceafc4166d857bdceaec255a57a3378c1e387
Alexander Shakhov <alexander.shakhov@sperasoft.com>
2018-05-08 10:48:25 +0300

    @InitBinder implementation for CustomerController

--- spring-mvc/src/main/java/com/udemy/spring/controller/CustomerController.java
+++ spring-mvc/src/main/java/com/udemy/spring/controller/CustomerController.java
@@ -1,9 +1,12 @@
 package com.udemy.spring.controller;
 
 import com.udemy.spring.model.Customer;
+import org.springframework.beans.propertyeditors.StringTrimmerEditor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.WebDataBinder;
+import org.springframework.web.bind.annotation.InitBinder;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -27,5 +30,19 @@ public class CustomerController {
         } else {
             return "customer-form";
         }
+    }
+
+    /**
+     * Method will cover every web request coming to this controller.
+     *
+     * Pre-process every String from data.
+     * Remove leading and trailing white space.
+     * If string only has white space it will trim it completely to 'null'.
+     * @param binder to register trimmer.
+     * */
+    @InitBinder
+    public void initBinder(WebDataBinder binder) {
+        StringTrimmerEditor stringTrimmerEditor = new StringTrimmerEditor(true);
+        binder.registerCustomEditor(String.class, stringTrimmerEditor);
     }
 }
--- spring-mvc/src/main/java/com/udemy/spring/model/Customer.java
+++ spring-mvc/src/main/java/com/udemy/spring/model/Customer.java
@@ -8,19 +8,17 @@ import javax.validation.constraints.Size;
 
 /**
  * @author alexander.shakhov on 07.05.2018 19:06
- * @project spring-basics
- * @description
  */
 @Getter
 @Setter
 public class Customer {
 
     @NotNull(message = "First name is required")
-    @Size(min = 2)
+    @Size(message = "Should not be shorter then 2 letters", min = 2)
     private String firstName;
 
 
     @NotNull(message = "Last name is required")
-    @Size(message = "Last name is not shorter then 2 letters", min = 2)
+    @Size(message = "Should not be shorter then 2 letters", min = 2)
     private String lastName;
 }
