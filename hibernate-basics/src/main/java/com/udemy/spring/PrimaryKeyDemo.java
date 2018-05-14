072e60466c1195aeff02a39533cee7e2d29a8c63
Alexander Shakhov <alexander.shakhov@sperasoft.com>
2018-05-14 13:10:29 +0300

    Create and Read

---
+++ hibernate-basics/src/main/java/com/udemy/spring/PrimaryKeyDemo.java
@@ -0,0 +1,48 @@
+package com.udemy.spring;
+
+import com.udemy.spring.entity.Student;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+/**
+ * @author alexander.shakhov on 14.05.2018 12:00
+ * @project spring-basics
+ * @description
+ */
+public class PrimaryKeyDemo {
+    public static void main(String[] args) {
+        SessionFactory factory = new Configuration()
+                .configure("hibernate.cfg.xml")
+                .addAnnotatedClass(Student.class)
+                .buildSessionFactory();
+
+        //create session
+        Session session = factory.getCurrentSession();
+
+        try{
+            //create 3 student objects
+            System.out.println("Creating 3 student objects...");
+            Student student1 = new Student("John", "Doe", "john@luv2code.com");
+            Student student2 = new Student("Mary", "Public", "mary@luv2code.com");
+            Student student3 = new Student("Bonita", "Applebum", "bonita@luv2code.com");
+
+            //start transaction
+            System.out.println("Starting transaction");
+            session.beginTransaction();
+
+            //save object
+            System.out.println("Saving students...");
+            session.save(student1);
+            session.save(student2);
+            session.save(student3);
+
+            //commit transaction
+            System.out.println("Committing multiple transactions");
+            session.getTransaction().commit();
+
+        } finally {
+            factory.close();
+        }
+    }
+}
---
+++ hibernate-basics/src/main/java/com/udemy/spring/ReadStudentDemo.java
@@ -0,0 +1,57 @@
+package com.udemy.spring;
+
+import com.udemy.spring.entity.Student;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+/**
+ * @author alexander.shakhov on 14.05.2018 13:00
+ * @project spring-basics
+ * @description
+ */
+public class ReadStudentDemo {
+    public static void main(String[] args) {
+        //create session factory
+        SessionFactory factory = new Configuration()
+                .configure("hibernate.cfg.xml")
+                .addAnnotatedClass(Student.class)
+                .buildSessionFactory();
+
+        //create session
+        Session session = factory.getCurrentSession();
+
+        try{
+            //create 3 student objects
+            System.out.println("Creating 3 student objects...");
+            Student daffy = new Student("Daffy", "Duck", "daffy@luv2code.com");
+
+            //start transaction
+            System.out.println("Starting transaction");
+            session.beginTransaction();
+
+            //save object
+            System.out.println("Saving students...");
+            session.save(daffy);
+
+            //commit transaction
+            System.out.println("Committing multiple transactions");
+            session.getTransaction().commit();
+
+            //retrieving object from DB
+            System.out.println("Get ID: " + daffy.getId());
+            session = factory.getCurrentSession();
+            session.beginTransaction();
+            Student student = session.get(Student.class, daffy.getId());
+            System.out.println("Get complete: " + student);
+
+            //commit the transaction
+            session.getTransaction().commit();
+
+            System.out.println("Done");
+
+        } finally {
+            factory.close();
+        }
+    }
+}
