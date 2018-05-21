9b98414c599b645ff640a66d846603c39a7ebeb4
Aleksandr Shakhov <write.me.messages@gmail.com>
2018-05-21 22:44:50 +0300

    aop

---
+++ spring-aop-after-finally/src/main/java/com/udemy/spring/AfterFinallyDemoApp.java
@@ -0,0 +1,54 @@
+package com.udemy.spring;
+
+import com.udemy.spring.config.ConfigApp;
+import com.udemy.spring.dao.AccountDao;
+import com.udemy.spring.model.Account;
+import java.util.List;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class AfterFinallyDemoApp {
+
+	public static void main(String[] args) {
+
+		// read spring config java class
+		AnnotationConfigApplicationContext context =
+				new AnnotationConfigApplicationContext(ConfigApp.class);
+
+		// get the bean from spring container
+		AccountDao accountDao = context.getBean("accountDao", AccountDao.class);
+
+		// call method to find the accounts
+		List<Account> accounts = null;
+
+		try {
+			// add a boolean flag to simulate exceptions
+			boolean tripWire = false;
+			accounts = accountDao.findAccounts(tripWire);
+		}
+		catch (Exception exc) {
+			System.out.println("\n\nMain Program ... caught exception: " + exc);
+		}
+
+		// display the accounts
+		System.out.println("\n\nMain Program: AfterThrowingDemoApp");
+		System.out.println("----");
+
+		System.out.println(accounts);
+
+		System.out.println("\n");
+
+		// close the context
+		context.close();
+	}
+
+}
+
+
+
+
+
+
+
+
+
+
---
+++ spring-aop-after-finally/src/main/java/com/udemy/spring/AfterReturningDemoApp.java
@@ -0,0 +1,53 @@
+package com.udemy.spring;
+
+import com.udemy.spring.config.ConfigApp;
+import com.udemy.spring.dao.AccountDao;
+import com.udemy.spring.model.Account;
+import java.util.List;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class AfterReturningDemoApp {
+
+	public static void main(String[] args) {
+
+		// read spring config java class
+		AnnotationConfigApplicationContext context =
+				new AnnotationConfigApplicationContext(ConfigApp.class);
+		
+		// get the bean from spring container
+		AccountDao accountDao = context.getBean("accountDao", AccountDao.class);
+
+		// call method to find the accounts
+		List<Account> theAccounts = null;
+
+		try {
+			// add a boolean flag to simulate exceptions
+			boolean tripWire = true;
+			theAccounts = accountDao.findAccounts(tripWire);
+		} catch (Exception exc) {
+			System.out.println("\n\nMain Program ... caught exception: " + exc);
+		}
+		
+		// display the accounts
+		System.out.println("\n\nMain Program: AfterReturningDemoApp");
+		System.out.println("----");
+		
+		System.out.println(theAccounts);
+		
+		System.out.println("\n");
+		
+		// close the context
+		context.close();
+	}
+
+}
+
+
+
+
+
+
+
+
+
+
